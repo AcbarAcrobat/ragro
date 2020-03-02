135f11f94232ab15600fdd07a584f35b8e20d7ca
artlv <you@example.com>
2020-03-02 17:13:01 +0300

    fix some problems

--- tests/get_api/test_get_dictionary_rfid_states.py
+++ tests/get_api/test_get_dictionary_rfid_states.py
@@ -4,9 +4,11 @@ from truth.truth import AssertThat
 from support.testdata import TestData
 from support.schema.dictionary_rdind_states_schema import schema
 from jsonschema import validate
+import logging
 
 
 T = TestData()
+LOGGER = logging.getLogger(__name__)
 
 
 @allure.parent_suite("GET request")
@@ -17,6 +19,9 @@ def test_get_dictionary_rfid_states():
         r = requests.get(T.url_() + "/get/dictionary/rfid_states")
     with allure.step("Assert status code is 200"):
         AssertThat(r.status_code).IsEqualTo(200)
+    with allure.step("LOGGER get info"):
+        LOGGER.info(r.json())
+        LOGGER.info(r.status_code)
     with allure.step("Validate server response according to our scheme"):
         validate(instance=r.json(), schema=schema)
     with allure.step("Assert Contains Items"):
@@ -25,4 +30,3 @@ def test_get_dictionary_rfid_states():
         AssertThat(r.json()["result"]).ContainsItem("NO_CONNECTION", 0)
         AssertThat(r.json()["result"]).ContainsItem("REMOVE_CARD", -1)
         AssertThat(r.json()["result"]).ContainsItem("NO_CARD", 1)
-    print(r.status_code)
