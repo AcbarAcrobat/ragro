4b20852be28ea18ca9bcf8dc342ff5f3952e110f
artlv <you@example.com>
2020-03-05 16:15:38 +0300

    add biseness logic cases

--- tests/get_api/biseness_tests/test_get_status_and_check_state_minus3.py
+++ tests/get_api/biseness_tests/test_get_status_and_check_state_minus3.py
@@ -1,5 +1,4 @@
 import time
-import json
 import requests
 import allure
 from truth.truth import AssertThat
@@ -11,12 +10,12 @@ T = TestData()
 LOGGER = logging.getLogger(__name__)
 
 
-@allure.feature("Test case check biseness logic")
+@allure.feature("Test case")
 @allure.story("Change unloader_bypass and check state")
 @allure.parent_suite("GET request")
 @allure.sub_suite("/get/status")
 @allure.title("Positive get request")
-def test_get_status_check_siren():
+def test_get_status_check_vehicle_state_minus3():
     with allure.step("Send requests to the MQTT"):
         mqtt.req(ename="RFID_1", etype="text", evalue="94594156156156")
         mqtt.req(ename="unloader_bypass", etype="switch", evalue="1")  # we wait state -3 in response
@@ -27,4 +26,5 @@ def test_get_status_check_siren():
         LOGGER.info(r.json())
         LOGGER.info(r.status_code)
     with allure.step("Assert Contains Item"):
-        AssertThat(r.json()["result"]).ContainsItem("state", -3)
+        with allure.step("State should be -3"):
+            AssertThat(r.json()["result"]).ContainsItem("state", -3)
