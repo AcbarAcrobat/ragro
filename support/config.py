a16a4fea29816394e8abdff5eea457a5f64c9297
Konstantin <aurococcus@gmail.com>
2020-03-12 17:08:47 +0700

    fix Pathlin open

--- support/config.py
+++ support/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 root_dir = Path(os.getcwd())
 
-with open(root_dir.joinpath('config.yml')) as f:
+with Path.open(root_dir.joinpath('config.yml')) as f:
     data = f.read()
 
 _config = yaml.safe_load(data)
