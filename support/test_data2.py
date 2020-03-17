1811b7eef6399e7ece818082c1caaacc5dac03e9
Artyom LV <artemsolomid@gmail.com>
2020-03-17 10:49:25 +0300

    clean name

--- support/test_data2.py
+++ support/test_data2.py
@@ -4,7 +4,14 @@ from support import config
 def murl():
     return config.get("murl")
 
+<<<<<<< HEAD
 def url83():
+=======
+def murl85():
+    return config.get("murl85")
+
+def url():
+>>>>>>> 2b3fa145f4d1bc12c3701fd2595b437ab00e1748
     return config.get("url")
 
 def url85():
